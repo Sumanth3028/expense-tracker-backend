daa26a7e794d67dda38a112f02f63c3b4a60f159
Sumanth3028 <sumanthakula05@gmail.com>
2023-07-10 15:01:26 +0530

    signup

---
+++ controllers/signup-controllers.js
@@ -0,0 +1,22 @@
+const sequelize=require('../util/database')
+
+const signup=require('../models/signup')
+
+exports.getSignupDetails=(req,res,next)=>{
+   signup.findAll().then(response=>{
+    res.json(response)
+   }).catch(err=>console.log(err))
+}
+
+exports.postSignupDetails=async(req,res,next)=>{
+
+  
+    const email=req.body.email
+    const password=req.body.password
+    const data=await signup.create({
+        
+        email:email,
+        password:password
+    })
+    res.status(201).json({getDetails:data})
+}
