d27fe92efdae4559558f6f54ebfae3fccee8723f
Sumanth3028 <sumanthakula05@gmail.com>
2023-07-22 18:09:21 +0530

    updating pass

--- routes/forgot-password-routes.js
+++ routes/forgot-password-routes.js
@@ -4,6 +4,10 @@ const router=express.Router()
 
 const forgotController=require('../controllers/forgotPassword-controllers')
 
+router.get('/updatepassword/:resetpasswordid', forgotController.updatePassword)
+
+router.get('/resetpassword/:id',forgotController.resetPassword)
+
 router.post('/forgotPassword',forgotController.forgotPassword)
 
 module.exports=router
